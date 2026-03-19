abd6a896ee713f033b0dc5c90c2810475fa890c5
Craig Ben Cadag <craigbencadag@gmail.com>
2026-03-19 11:39:10 +0800

    update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello, CI/CD is working!');
+    res.status(200).send('Hello, CI/CD is automated!');
 })
 
 if (require.main === module) {
